6a96722010db79d852523768529a6d3a98e3fb0c
shu.yamaga <s.yamaga.0318@gmail.com>
2025-12-14 07:57:30 +0000

    Create browser.rb
    add test method

--- test/iterator/browser_test.rb
+++ test/iterator/browser_test.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 
 require 'minitest/autorun'
+require_relative '../../lib/iterator/browser'
 
 class BrowserTest < Minitest::Test
-  def test_can_create_browser
-    browser = new Browser
+  def setup
+    @browser = Browser.new
+  end
+
+  def test_can_open_bookmark_nav
+    @browser.open_bookmark_nav
   end
 end
